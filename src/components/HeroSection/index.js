7b8d8eeac507551715475932449055b5b5e26744
Balogun Razak <razak2free@gmail.com>
2022-11-01 15:48:10 +0100

    02

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Button from '../common/Button'
+import './heroSection.css'
 
 const HeroSection = () => {
   return (
@@ -10,6 +12,7 @@ const HeroSection = () => {
         <div className='hero-subheading'>
            join 9M+ members who win rewards and cashbacks everyday
         </div>
+        <Button buttonText='Download Cred'/>
       </div>
     </div>
   )
