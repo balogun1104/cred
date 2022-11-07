668ac1bc77e6dd33e25e6c22cdbdc75b6904b123
Balogun Razak <razak2free@gmail.com>
2022-11-07 08:20:50 +0100

    Monday

--- src/components/ProductShowCase/index.js
+++ src/components/ProductShowCase/index.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './productShowCase.css'
 
 const ProductShowCase = () => {
+    const [showAnimation, setShowAnimation] = useState(false);
+    const observerRef = useRef(null);
+
+    const toggleAnination = (e) => {
+        if(e[0]?.isIntersecting){
+            setShowAnimation(true)
+        }
+    }
+
+    const options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.5,
+    }
+
+    useEffect(() => {
+        const observer = new IntersectionObserver (toggleAnination, options);
+
+        if(!showAnimation) {
+            if(observerRef.current) {
+                observer.observe(observerRef.current)
+            }
+        }
+
+        return () => {
+            if (observerRef.current) {
+                observer.unobserve(observerRef.current)
+            }
+        }
+    })
+
   return (
-    <div className='product-showcase'>
-        <div className='showcase-wrapper'>
+    <div className={`product-showcase  ${showAnimation ? 'scale-in-bottom' : '' }`}
+    ref={observerRef}
+    >
+       {showAnimation && ( <div className='showcase-wrapper'>
             <img src='https://web-images.credcdn.in/_next/assets/images/home-page/phone/neopop-left-2.png' 
             className='showcase-ui showcase-mockup-1'
             />
@@ -20,7 +53,7 @@ const ProductShowCase = () => {
             <img src='https://web-images.credcdn.in/_next/assets/images/home-page/phone/neopop-right-2.png' 
             className='showcase-ui showcase-mockup-5'
             />
-        </div>
+        </div>)}
     </div>
   )
 }
